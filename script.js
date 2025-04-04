986ed5a8ac94f7ec6744edaee32373f627740449
Joshua Mononoetoe <585732+DryRoastedLemon@users.noreply.github.com>
2025-04-04 13:28:05 +0200

    Added score variables

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getComputerChoice() {
+  // Randomly determines rock, paper or scissors for the computer.
+  // Returns a string.
   const choice = Math.floor(Math.random() * 3);
   if (choice === 0) return "rock";
   else if (choice === 1) return "paper";
@@ -6,6 +8,10 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
+  // Gets the human choice via a prompt. Returns a lowercase string.
   const choice = prompt("Choose your weapon: rock, paper or scissors");
   return choice.toLowerCase();
 }
+
+let humanScore = 0;
+let computerScore = 0;
